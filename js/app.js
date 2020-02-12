5ee4cc7b7d28287fb57802f2fa40e2a71334fbf5
gonzalosola@gmail.com <gonzalosola@gmail.com>
2020-02-12 02:21:07 -0300

    Nuevo logo. Hice aparecer una de las imagenes predeterminada

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 var renderer;
 var scene;
 var camera;
@@ -8,15 +7,26 @@ function init() {
     scene = new THREE.Scene();
     
     renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0x000000, 1.0);
+    renderer.setClearColor(0xdfdfdf, 1.0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    //renderer.setSize(800, 640); TODO: tamaño fijo
+    
+    //camera = new THREE.PerspectiveCamera(30, 800/640, 0.1, 1000);TODO: tamaño fijo
+
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     camera.position.x = 15;
     camera.position.y = 16;
     camera.position.z = 13;
+
+    var box = new THREE.BoxGeometry(3,2,3);
+    var material = new THREE.MeshBasicMaterial({color: 0xff0000})
+    var cube = new THREE.Mesh(box, material);
+    cube.castShadow = true;
+    scene.add(cube);
+
     camera.lookAt(scene.position);    
     document.body.appendChild(renderer.domElement);    
     render();    
