72be9970c2c2dafb5780f3f127538bee073fe9bb
gonzalosola@gmail.com <gonzalosola@gmail.com>
2020-02-13 01:32:32 -0300

    Agrego un dae, modelo de wally. Y se logra mostrar algo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-var renderer;
-var scene;
-var camera;
+var renderer, scene, camera, mesh, material;
 
 function init() {
 
@@ -10,32 +8,42 @@ function init() {
     renderer.setClearColor(0xdfdfdf, 1.0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
+    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
 
     //renderer.setSize(800, 640); TODO: tamaño fijo
-    
     //camera = new THREE.PerspectiveCamera(30, 800/640, 0.1, 1000);TODO: tamaño fijo
-
     camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-
     camera.position.x = 15;
     camera.position.y = 16;
     camera.position.z = 13;
-
     var box = new THREE.BoxGeometry(3,2,3);
     var material = new THREE.MeshBasicMaterial({color: 0xff0000})
     var cube = new THREE.Mesh(box, material);
-    cube.castShadow = true;
     scene.add(cube);
-
+    var loader = new THREE.ColladaLoader();
+    loader.load('../assets/model.dae', function (collada) {
+      scene.add(collada.scene);
+    } ); 
     camera.lookAt(scene.position);    
-    document.body.appendChild(renderer.domElement);    
-    render();    
+    document.body.appendChild(renderer.domElement);   
+    render();
+    animate();
+}
 
+function animate() {
+	requestAnimationFrame( animate );
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+	renderer.render( scene, camera );
 }
 
 function render() {
-    renderer.render(scene, camera);
     requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
 
 window.onload = init;
